cd4f2f3e73e958de5e4c63f27ecd83f3ed995e29
Ethan Setnik <esetnik@gmail.com>
2015-09-29 00:05:30 -0400

    first commit

---
+++ dist/angular-autosize.js
@@ -0,0 +1,32 @@
+/*
+ * ngDialog - easy modals and popup windows
+ * http://github.com/likeastore/ngDialog
+ * (c) 2013-2015 MIT License, https://likeastore.com
+ */
+
+(function(root, factory) {
+  if (typeof module !== 'undefined' && module.exports) {
+    // CommonJS
+    if (typeof angular === 'undefined') {
+      module.exports = factory(require('angular'));
+    } else {
+      module.exports = factory(angular);
+    }
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['ngAutosize'], function(angular) {
+      factory(root.angular);
+    });
+  } else {
+    // Global Variables
+    factory(root.angular);
+  }
+}(this, function(angular) {
+  'use strict';
+  return angular.module('ngAutosize', [])
+    .directive('autosize', function() {
+      return function(scope, element) {
+        autosize(element);
+      };
+    });
+}));
